cfe2a73afa467b989569a8725008055118fb88b6
Yewon Lee <to.jessywlee@gmail.com>
2025-01-02 14:37:05 +0900

    [feat] router, tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
-  return <div>spotadmin</div>;
+  return (
+    <Router>
+      <div className="text-center">
+        <img
+          src="src/assets/spotfinder_logo.png"
+          alt="spotfinder_logo"
+          className="w-10 h-10 m-auto"
+        />
+        <div className="text-[#4D63FC] font-semibold text-lg">
+          SPOT FINDER 회원 탈퇴
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/delete-account" element={<>회원탈퇴</>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
